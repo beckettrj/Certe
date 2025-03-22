a827e3788f9fd9b354d9c99888c19064423923ca
Rod Beckett <beckettrj@gmail.com>
2025-03-21 21:56:10 -0600

    Simplify app.py for deployment testing

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Railway!!!!!"
+    return "Hello from Railway---!"
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))  # default to 8080 just in case
     app.run(debug=True, host='0.0.0.0', port=port)
