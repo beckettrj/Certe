fe4f5c9781ba86ecf5095612d087235053f47906
Rod Beckett <beckettrj@gmail.com>
2025-03-21 21:25:59 -0600

    Simplify app.py for deployment testing

--- app.py
+++ app.py
@@ -9,14 +9,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     logging.info("✅ Index route was hit")
-    return "<h1>✅ Certe is finally LIVE!</h1>"
-
-@app.route('/favicon.ico')
+    return "<h1>✅ Certe is finally LIVE!</h1><BR><H2> test 123</H2>"
+    
 def favicon():
     return '', 204
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     logging.info(f"Starting app on port {port}")
-    app.run(host="0.0.0.0", port=port)
-
+    app.run(host="0.0.0.0", port=port)  
