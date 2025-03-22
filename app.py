81f65f5340f9cf80fe9742832b6cb12e2cf35716
Rod Beckett <beckettrj@gmail.com>
2025-03-22 00:59:25 -0600

    Simplify app.py for deployment testing

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Railway!"
+    return "Hello from Railway111!"
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', '5000'))
     print("🟢 Flask running on port:", port)  # Debug log for Railway
     app.run(host='0.0.0.0', port=port)
+
