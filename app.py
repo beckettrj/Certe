654635db93de7a280f17fe677ba2231e9f6f41d5
Rod Beckett <beckettrj@gmail.com>
2025-03-21 21:18:25 -0600

    Log route hit + add favicon route

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     logging.info("✅ Index route was hit")
-    return "<h1>✅ Certe is deployed! Template test skipped.</h1>"
+    return "<h1>✅ Certe is finally LIVE!</h1>"
 
 @app.route('/favicon.ico')
 def favicon():
@@ -19,3 +19,4 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     logging.info(f"Starting app on port {port}")
     app.run(host="0.0.0.0", port=port)
+
